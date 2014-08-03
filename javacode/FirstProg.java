7436c0711835a5667f56b66688e1b8a74db43ebc
nyghtowl <warrick.melanie@gmail.com>
2014-08-02 22:38:43 -0700

    Initial setup and first java program run.

---
+++ javacode/FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg
+{
+	public static void main(String[] args)
+	{
+		System.out.println( "I am determined to learn more stuff." );
+		System.out.println( "Today's date is" );
+	}
+}
