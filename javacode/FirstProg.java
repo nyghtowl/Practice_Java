d92ba60511089e99d60a4c890540f649a6dc4501
nyghtowl <warrick.melanie@gmail.com>
2014-08-03 11:18:59 -0700

    Moved.

--- javacode/FirstProg.java
+++
@@ -1,8 +0,0 @@
-public class FirstProg
-{
-	public static void main(String[] args)
-	{
-		System.out.println( "I am determined to learn more stuff." );
-		System.out.println( "Today's date is" );
-	}
-}
